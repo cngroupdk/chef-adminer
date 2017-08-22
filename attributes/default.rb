79e2d7ecd80b7004cbaf9b712e6f2913a824f5f9
terry chay <terry@raise.me>
2017-08-22 15:05:26 -0700

    updated stuff to work again (+ other changes)
    - repo moved from sourceforge to github
    - allows owner and group to be configurable (from root)
    - updated from 4.2.2 to 4.3.1
    - added `general_intl` version
    - plugins installs just like in https://www.adminer.org/en/plugins
    
    Signed-off-by: terry chay <terry@raise.me>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,13 @@
 default['adminer']['dir_to_install'] = '/var/www/db'
 
 default['adminer']['version_to_install'] = 'mysql_english'
+default['adminer']['user']  = 'root'
+default['adminer']['group'] = 'root'
 
 default['adminer']['versions_available'] = {
-  :mysql_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql-en.php',
-  :mysql_intl => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql.php',
-  :general_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-en.php'
+  :mysql_english   => 'https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql-en.php',
+  :mysql_intl      => 'https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php',
+  :general_intl    => 'https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1.php',
+  :general_english => 'https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-en.php'
 }
 
